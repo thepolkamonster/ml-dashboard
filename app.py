f8ca0c8f49ca347ad0b1855e75e0368e8af61b6c
thepolkamonster <sakshamparashar12@gmail.com>
2023-09-22 20:34:16 +0530

    Created files and added highlight.js code to elements.html

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_smorest import Api
+
+def create_app():
+    app = Flask(__name__)
+    app.config["API_TITLE"] = "Dash-ML"
+    app.config['SECRET_KEY'] = 'key'
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    api = Api(app)
+    return app
+
